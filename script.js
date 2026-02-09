89df50a061c5cd3dd13dd37c3d14be19198f5c63
Himanshu Singh <himanshusingh1k@gmail.com>
2026-02-09 10:27:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function findLargest(a, b, c) {
-  //your code here
+  if (a > b) {
+    if (a > c) return a;
+    return c;
+  } else {
+    if (b > c) return b;
+    return c;
+  }
 }
 
 const num1 = parseInt(prompt("Enter First Number."));
 const num2 = parseInt(prompt("Enter Second Number."));
 const num3 = parseInt(prompt("Enter Third Number."));
+
 alert(findLargest(num1, num2, num3));
